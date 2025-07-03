73440d21b927fcc7b0d0533a8b3e6e60dabac23d
ajk0911 <Arvidjkarlsson@hotmail.com>
2025-07-03 14:33:54 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Query
-from scraper import scrape_dack
-
-app = FastAPI()
-
-@app.get("/dack")
-def get_dack(dacktyp: str = Query(...), dimension: str = Query(...)):
-    resultat = scrape_dack(dacktyp, dimension)
-    return {"resultat": resultat}
